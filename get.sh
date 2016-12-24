39fe868f5511e7844815d2498e07884794fd3185
Russley Shaw <russleyshaw@gmail.com>
2016-12-24 12:14:42 -0600

    Remove interactivity

--- get.sh
+++ get.sh
@@ -5,9 +5,4 @@ rm -rf ${DIR}
 git clone https://github.com/russleyshaw/dotfiles.git ${DIR}
 cd ${DIR}
 
-echo "Which configuration do you want to use?"
-select config in "laptop" "desktop"; do
-    export DOTFILE_CONFIG=config
-done
-
 bash ./install.sh
--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
-echo $config
-
 # Setup folders
 mkdir -p ~/bin/
 mkdir -p ~/images/screenshots/
