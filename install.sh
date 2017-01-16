2abcc3124f31a9c6990665689ee53cfeb95d9681
Russley Shaw <russleyshaw@gmail.com>
2017-01-16 03:19:23 +0000

    Working on disk partitioning

--- install.sh
+++ install.sh
@@ -23,12 +23,24 @@ function prompt_menu {
     echo $choices
 }
 
-function prompt_menu_adv {
-    #title, text, keys, options
+function prompt_menu_raw {
+    #title, text, options
     MENU_OPTIONS=
     COUNT=0
     
-    MENU_OPTIONS=$(array_zip $3 $4)
+    MENU_OPTIONS=$3
+    cmd=(dialog --clear --title "$1" --menu "$2" 22 76 16)
+    menu_options=(${MENU_OPTIONS})
+    choices=$("${cmd[@]}" "${menu_options[@]}" 2>&1 >/dev/tty)
+    echo $choices
+}
+
+function prompt_menu_zip {
+    #title, text, ids, text
+    MENU_OPTIONS=
+    COUNT=0
+    
+    MENU_OPTIONS=array_zip $3 $4
     cmd=(dialog --clear --title "$1" --menu "$2" 22 76 16)
     menu_options=(${MENU_OPTIONS})
     choices=$("${cmd[@]}" "${menu_options[@]}" 2>&1 >/dev/tty)
@@ -58,40 +70,65 @@ function prompt_pause {
     dialog --clear --title "$1" --pause "$2" 22 76 $3
 }
 
+################################################################################
 # Welcome to Rinux
+################################################################################
 prompt_msg "Welcome to Rinux" "This script will guide you through installing your new Arch Linux installation"
 
 ### Pre-Installation
 
-## Keyboard Layout
-unset choices
-choices=$(prompt_menu "Keyboard Layout" "Select a keyboard layout: " "$(ls /usr/share/kbd/keymaps/**/*.map.gz | xargs basename -s .map.gz | sort | uniq)")
-if [ -n "$choices" ]
-then
-    prompt_msg "Keyboard Layout" "You chose the layout: $choices."
-    loadkeys $choices
-else
-    prompt_msg "Keyboard Layout" "You chose no layout."
-fi
+################################################################################
+# Keyboard Layout
+################################################################################
+# unset choices
+# choices=$(prompt_menu "Keyboard Layout" "Select a keyboard layout: " "$(ls /usr/share/kbd/keymaps/**/*.map.gz | xargs basename -s .map.gz | sort | uniq)")
+# if [ -n "$choices" ]
+# then
+#     prompt_msg "Keyboard Layout" "You chose the layout: $choices."
+#     loadkeys $choices
+# else
+#     prompt_msg "Keyboard Layout" "You chose no layout."
+# fi
     
-## Internet
+################################################################################
+# Internet
+################################################################################
 prompt_progress "Internet Configuration" "ping -c 10 www.google.com"
 
-## Time
-unset choices
-choices=$(prompt_menu "Time Configuration" "Select a timezone: " "$(timedatectl list-timezones | sort | uniq)")
-if [ -n "$choices" ]
+################################################################################
+# Time
+################################################################################
+unset choice
+choice=$(prompt_menu "Time Configuration" "Select a timezone: " "$(timedatectl list-timezones | sort | uniq)")
+if [ -n "$choice" ]
 then
-    prompt_msg "Time Configuration" "You chose the timezone: $choices."
-    timedatectl set-timezone $choices
+    prompt_msg "Time Configuration" "You chose the timezone: $choice."
+    timedatectl set-timezone $choice
     timedatectl set-ntp true
-    prompt_msg "Time Configuration" "$(timedatectl status)"
-else
-    prompt_pause "Keyboard Layout" "You chose no timezone." 5
-    prompt_msg "Time Configuration" "$(timedatectl status)"
 fi
+prompt_msg "Time Configuration" "$(timedatectl status)"
 
+################################################################################
+# Disk Partitioning
+################################################################################
+unset choice
+choice=$(prompt_menu_raw "Disk Partitioning" "Select a partitioning method: " "simple 'Entire disk, single root, no swap' diy 'Do It Yourself (bash)'")
+case $choice in
+    simple)
+        devmem=$(fdisk --list | grep "^Disk " | cut -d, -f1 | sed "s/Disk\ //;s/\ //g;s/:/\ /")
+        choice=$(prompt_menu_raw "Disk Partitioning" "Select a disk to install to: " "$devmem")
+        prompt_msg "Disk Partitioning" "SELECTED $choice"
+        ;;
+    diy)
+        prompt_msg "Disk Partitioning" "Please partition your disks manually. A bash prompt will open. Please exit when finished"
+        bash
+        ;;
+esac
+
+
+################################################################################
 # Reboot?
+################################################################################
 unset choice
 choice=$(prompt_yesno "Installation Complete" "Your installation of Rinix is complete. Reboot?")
 prompt_msg "Keyboard Layout" "You chose the layout: $choice."
@@ -100,7 +137,7 @@ then
     prompt_pause "Installation Complete" "Rebooting..." 5
     reboot
 fi
-
+clear
 exit 0
 
 
